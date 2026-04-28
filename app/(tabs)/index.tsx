c9d39999d91c36cad828175246711d1f6fba4ee1
Quang <quangtmhe186215@fpt.edu.vn>
2026-04-28 19:01:48 +0700

    array

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,37 +1,48 @@
 
 import { useState } from 'react';
-import { Text, View, StyleSheet, Button, TextInput} from 'react-native';
-import { blue } from 'react-native-reanimated/lib/typescript/Colors';
+import { Text, View, StyleSheet, ScrollView } from 'react-native';
 
 export default function HomeScreen() {
 
-  const [count, setCount] = useState<number>(0);
-  const [name,setName] = useState<string>("");
+  const [students, setStudents] = useState([
+    { id: 1, age: 20, name: "Quang" },
+    { id: 2, age: 21, name: "Nam" },
+    { id: 3, age: 19, name: "Huy" },
+    { id: 4, age: 22, name: "Minh" },
+    { id: 5, age: 20, name: "Tuan" },
+    { id: 6, age: 23, name: "Dat" },
+    { id: 7, age: 18, name: "Khanh" },
+    { id: 8, age: 21, name: "Long" },
+    { id: 9, age: 20, name: "Duc" },
+    { id: 11, age: 19, name: "Nghĩa" },
+    { id: 12, age: 19, name: "Chấn" },
+    { id: 13, age: 19, name: "Sang" },
+    { id: 14, age: 19, name: "Thắng" }
+  ]);
+
 
   return (
-    <View style={styles.container}>
-       <Text>Name: {name}</Text>
-       <TextInput onChangeText={(value) => setName(value)}
-       style={{borderWidth: 1,
-                borderColor: "blue", width : 200, padding : 20
-       }}></TextInput>
-       <Text>
-            Count = {count}
-       </Text>
-       <View>
-            <Button title='Tăng' onPress={() => setCount(count + 1)}></Button>
-            <Button title='Giamr' onPress={() => setCount(count - 1)}></Button>
-       </View>
+    <View>
+      <ScrollView>
+        {students.map(item => {
+          return (
+            <View key={item.id} style={styles.container}>
+              <Text>{item.name} - {item.age}</Text>
+            </View>
+          )
+        })}
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
+    padding: 3,
+    backgroundColor: "pink",
+    marginBottom: 15,
+    fontWeight: "bold",
+    height: 60,
   },
   text: {
     fontSize: 30,
@@ -42,11 +53,11 @@ const styles = StyleSheet.create({
     fontSize: 18,
     color: 'gray',
   },
-  hello1 : {
+  hello1: {
     color: "red", fontSize: 25,
     borderWidth: 1
   },
-  header:{
-    fontSize: 30, color:"blue", fontWeight: "bold"
+  header: {
+    fontSize: 30, color: "blue", fontWeight: "bold"
   }
 });
